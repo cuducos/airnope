0819e649b34cacc94c13c5db14cc4b34876dd187
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-08-04 20:04:00 -0400

    Adds LRU cache to embeddings

--- src/embedding.rs
+++ src/embedding.rs
@@ -1,9 +1,11 @@
 use anyhow::{anyhow, Result};
+use lru::LruCache;
 use rust_bert::pipelines::sentence_embeddings::{
     SentenceEmbeddingsConfig, SentenceEmbeddingsModel, SentenceEmbeddingsModelType,
 };
 use std::fs;
 use std::io::{ErrorKind, Read, Write};
+use std::num::NonZeroUsize;
 use std::os::unix::net::{UnixListener, UnixStream};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::UnixStream as TokioUnixStream;
@@ -11,22 +13,44 @@ use tokio::net::UnixStream as TokioUnixStream;
 const PATH: &str = ".airnope.embedding.socket";
 const EOL: &[u8] = b"\n";
 
+fn to_array(vector: &[f32]) -> Result<[f32; 384]> {
+    if vector.len() != 384 {
+        return Err(anyhow!("Embedding does not have 384 numbers"));
+    }
+    let mut result = [0 as f32; 384];
+    for (idx, num) in vector.iter().enumerate() {
+        result[idx] = *num;
+    }
+    Ok(result)
+}
+
 pub struct ZeroShotClassification {
     model: SentenceEmbeddingsModel,
+    cache: LruCache<Vec<u8>, [f32; 384]>,
 }
 
 impl ZeroShotClassification {
     pub fn new() -> Result<Self> {
         let config = SentenceEmbeddingsConfig::from(SentenceEmbeddingsModelType::AllMiniLmL6V2);
         let model = SentenceEmbeddingsModel::new(config)?;
-        Ok(Self { model })
+        let cache = LruCache::new(
+            NonZeroUsize::new(1024).ok_or(anyhow!("Could not create LRU cache size"))?,
+        );
+        Ok(Self { model, cache })
     }
 
-    fn embedding(&self, text: String) -> Result<String> {
-        let embeddings = self.model.encode(&[text])?;
-        let vector = embeddings
-            .first()
-            .ok_or(anyhow!("Error creating embedding"))?;
+    fn embedding(&mut self, text: String) -> Result<String> {
+        let key = text.as_bytes().to_vec();
+        let vector = match self.cache.get(&key) {
+            Some(v) => *v,
+            None => {
+                let results = self.model.encode(&[text])?;
+                let result = results.first().ok_or(anyhow!("Error creating embedding"))?;
+                let embeddings = to_array(result)?;
+                self.cache.put(key, embeddings);
+                embeddings
+            }
+        };
 
         Ok(vector
             .iter()
@@ -36,7 +60,7 @@ impl ZeroShotClassification {
     }
 }
 
-fn handle_request(model: &ZeroShotClassification, mut stream: UnixStream) {
+fn handle_request(model: &mut ZeroShotClassification, mut stream: UnixStream) {
     let mut buffer = [0; 512];
     loop {
         match stream.read(&mut buffer) {
@@ -79,14 +103,14 @@ pub fn serve() -> Result<()> {
         fs::remove_file(PATH)?;
     }
 
-    let model = ZeroShotClassification::new()?;
+    let mut model = ZeroShotClassification::new()?;
     let listener = UnixListener::bind(PATH)?;
     log::info!("Embedding server listening on {}", PATH);
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_request(&model, stream);
+                handle_request(&mut model, stream);
             }
             Err(e) => {
                 log::error!("Connection to embedding server failed: {}", e);
@@ -112,16 +136,13 @@ pub async fn embedding_for(text: &str) -> Result<[f32; 384]> {
     let mut reader = BufReader::new(&mut stream);
     let mut response = vec![];
     reader.read_until(EOL[0], &mut response).await?;
+
     let text = String::from_utf8_lossy(&response);
-    let mut result = [0 as f32; 384];
-    for (idx, num) in text.split(',').enumerate() {
-        if idx >= 384 {
-            return Err(anyhow!("Embedding got more than 384 numbers"));
-        }
-        match num.trim().parse::<f32>() {
-            Ok(n) => result[idx] = n,
-            Err(e) => return Err(anyhow!("Cannot convert {:?} to `f32`: {}", num, e)),
-        }
-    }
+    let result = to_array(
+        &text
+            .split(',')
+            .filter_map(|n| n.trim().parse().ok())
+            .collect::<Vec<f32>>(),
+    )?;
     Ok(result)
 }
